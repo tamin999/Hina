e902b94f091a8ccce69a24b5c779baae9d802be1
Raihan Arexx <bytebrexx3@gmail.com>
2025-07-20 01:16:49 +0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -536,6 +536,9 @@ async function getStreamFromURL(url = "", pathName = "", options = {}) {
 			url,
 			method: "GET",
 			responseType: "stream",
+			headers: {
+				"user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
+			},
 			...options
 		});
 		if (!pathName)
