fff151f5fb5e661bd461a211d4f40547b2cc9e30
Raihan Arexx <bytebrexx3@gmail.com>
2025-07-23 19:24:45 +0600

    Update pair2.js

--- scripts/cmds/pair2.js
+++ scripts/cmds/pair2.js
@@ -1,64 +1,110 @@
-const { getStreamFromURL } = global.utils;
+const { loadImage, createCanvas } = require("canvas");
+const axios = require("axios");
+const fs = require("fs-extra");
 
 module.exports = {
   config: {
     name: "pair",
-    version: "1.0",
-    author: "Rulex-al Loufi",
-    shortDescription: {
-      in: "pair Girls 😗",
-      vi: ""
-    },
-    category: "fun",
-    guide: "{prefix}random-female"
+    aurthor:"Arafat",
+     role: 0,
+    shortDescription: " ",
+    longDescription: "",
+    category: "love",
+    guide: "{pn}"
   },
+  onStart: async function ({ api, event, args, usersData, threadsData }) {
+    api.setMessageReaction("💝", event.messageID, (err) => {}, true);
+    let pathImg = __dirname + "/cache/background.png";
+    let pathAvt1 = __dirname + "/cache/Avtmot.png";
+    let pathAvt2 = __dirname + "/cache/Avthai.png";
 
-  onStart: async function({ api, event, threadsData, message, usersData }) {
-    const uidI = event.senderID;
-    let dataSender = await api.getUserInfo(uidI);
-    const name1 = await dataSender[uidI].name;
-    const avatarUrl1 = `https://graph.facebook.com/${uidI}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+    var id1 = event.senderID;
+    var name1 = ""; // Replace with function that retrieves the name of the user
+    var ThreadInfo = await api.getThreadInfo(event.threadID);
+    var all = ThreadInfo.userInfo;
+    for (let c of all) {
+      if (c.id == id1) var gender1 = c.gender;
+    }
+    const botID = api.getCurrentUserID();
+    let ungvien = [];
+    if (gender1 == "FEMALE") {
+      for (let u of all) {
+        if (u.gender == "MALE") {
+          if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
+        }
+      }
+    } else if (gender1 == "MALE") {
+      for (let u of all) {
+        if (u.gender == "FEMALE") {
+          if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
+        }
+      }
+    } else {
+      for (let u of all) {
+        if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
+      }
+    }
+    var id2 = ungvien[Math.floor(Math.random() * ungvien.length)];
+    var name2 = "Uff ksto ramro jodi 💋"; // Replace with function that retrieves the name of the user
+    var rd1 = Math.floor(Math.random() * 100) + 1;
+    var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
+    var rd2 = cc[Math.floor(Math.random() * cc.length)];
+    var djtme = [`${rd1}`, ${rd1}, ${rd1}, ${rd1}, ${rd1}, ${rd2}, ${rd1}, ${rd1}, ${rd1}, `${rd1}`];
 
-    const threadData = await threadsData.get(event.threadID);
-    const members = threadData.members.filter(member => member.inGroup);
-    const senderGender = threadData.members.find(
-      member => member.userID === uidI
-    )?.gender;
+    var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
-    if (members.length === 0)
-      return message.reply("There are no members in the group ☹️💕😢");
+    var background = [
+      "https://files.catbox.moe/ar1iz7.jpg",
+      "https://files.catbox.moe/ar1iz7.jpg",
+      "https://files.catbox.moe/ar1iz7.jpg",
+    ];
+    var rd = background[Math.floor(Math.random() * background.length)];
+    let getAvtmot = (
+      await axios.get(https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662, {
+        responseType: "arraybuffer",
+      })
+    ).data;
+    fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
+    let getAvthai = (
+      await axios.get(https://graph.facebook.com/${id2}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662, {
+        responseType: "arraybuffer",
+      })
+    ).data;
+    fs.writeFileSync(pathAvt2, Buffer.from(getAvthai, "utf-8"));
 
-    const eligibleMembers = members.filter(
-      member => member.gender !== senderGender
+    let getbackground = (
+      await axios.get(${rd}, {
+        responseType: "arraybuffer",
+      })
+    ).data;
+    fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
+
+    let baseImage = await loadImage(pathImg);
+    let baseAvt1 = await loadImage(pathAvt1);
+    let baseAvt2 = await loadImage(pathAvt2);
+    let canvas = createCanvas(baseImage.width, baseImage.height);
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(baseAvt1, 100, 150, 300, 300);
+    ctx.drawImage(baseAvt2, 900, 150, 300, 300);
+    const imageBuffer = canvas.toBuffer();
+    fs.writeFileSync(pathImg, imageBuffer);
+    fs.removeSync(pathAvt1);
+    fs.removeSync(pathAvt2);
+    return api.sendMessage(
+      {
+        body: 🥰Successful pairing! ${name1}\💌Wish you two hundred years of happiness💕${name2}.\—The odds are ${tile}%,
+        mentions: [
+          {
+            tag: ${name2},
+            id: id2,
+          },
+        ],
+        attachment: fs.createReadStream(pathImg),
+      },
+      event.threadID,
+      () => fs.unlinkSync(pathImg),
+      event.messageID
     );
-    if (eligibleMembers.length === 0)
-      return message.reply(
-        "There are no male/other members in the group ☹️💕😢"
-      );
-
-    const randomIndex = Math.floor(Math.random() * eligibleMembers.length);
-    const randomMember = eligibleMembers[randomIndex];
-    let dataRandomMember = await api.getUserInfo(randomMember.userID);
-    const name2 = await dataRandomMember[randomMember.userID].name;
-    const avatarUrl2 = `https://graph.facebook.com/${randomMember.userID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-
-    const randomNumber1 = Math.floor(Math.random() * 36) + 65;
-    const randomNumber2 = Math.floor(Math.random() * 36) + 65;
-
-    message.reply({
-      body: `•『💗』Congratulations  💕 the new husband and wife: \n 🌸  ${name1}
-  💕 ${name2} \n
-        Like Ratio: "${randomNumber1}% 🤭"
-        Like Rate: "${randomNumber2} % 💕"
-        \nCongratulations 🌝`,
-      mentions: [
-        { id: uidI, tag: name1 },
-        { id: randomMember.userID, tag: name2 }
-      ],
-      attachment: [
-        await getStreamFromURL(avatarUrl1),
-        await getStreamFromURL(avatarUrl2)
-      ]
-    });
-  }
+  },
 };
