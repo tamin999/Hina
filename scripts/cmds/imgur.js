338f21790d288edc7f62de618fdea7526c6435fc
gandu1962 <nuralam66668@gmail.com>
2025-08-22 03:49:31 -0600

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,39 +1,53 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-    config: {
-        name: "imgur",
-        version: "1.0",
-        role: 0,
-        author: "♡ 𝐻𝐴𝑆𝐴𝑁 ♡",
-        shortDescription: "imgur upload",
-        countDown: 0,
-        category: "tools",
-        guide: {
-            en: '[reply to image]'
+  config: {
+    name: "imgur",
+    author: "MahMUD",
+    version: "1.7",
+    category: "media"
+  },
+  onStart: async function ({ api, event }) {
+    try {
+       const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68);
+      if (this.config.author !== obfuscatedAuthor) {
+        return api.sendMessage(
+          "You are not authorized to change the author name.\n\nPlease author fix name to work with this cmd",
+          event.threadID,
+          event.messageID
+        );
+      }
+
+       if (!event.messageReply || !event.messageReply.attachments) {
+        return api.sendMessage(
+          "❌ Please reply to an image or video message to upload it to Imgur.",
+          event.threadID,
+          event.messageID
+        );
+      }
+
+      const attachment = event.messageReply.attachments[0].url;
+
+        const response = await axios.post(
+        "https://api.imgur.com/3/image",
+        { image: attachment, type: "url" },
+        {
+          headers: {
+            Authorization: "Client-ID 137256035dcfdcc"
+          }
         }
-    },
+      );
 
-    onStart: async ({ api, event }) => {
-        let link2;
+      const imgurLink = response.data.data.link;
+      api.sendMessage(`${imgurLink}`, event.threadID, event.messageID);
 
-        if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
-            link2 = event.messageReply.attachments[0].url;
-        } else if (event.attachments.length > 0) {
-            link2 = event.attachments[0].url;
-        } else {
-            return api.sendMessage('No attachment detected. Please reply to an image.', event.threadID, event.messageID);
-        }
-
-        try {
-
-    const toxiciter = "https://rest-nyx-apis.onrender.com/api";
-            const res = await axios.get(`${toxiciter}/imgur?url=${encodeURIComponent(link2)}`);
-            const link = res.data.data;
-            return api.sendMessage(`\n\n${link}`, event.threadID, event.messageID);
-        } catch (error) {
-            console.error("Error uploading image to Imgur:", error);
-            return api.sendMessage("An error occurred while uploading the image to Imgur.", event.threadID, event.messageID);
-        }
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+      api.sendMessage(
+        `❌ Failed to upload to Imgur.\n${error.response?.data?.data?.error || error.message}`,
+        event.threadID,
+        event.messageID
+      );
     }
+  }
 };
