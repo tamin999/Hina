134126af7ac7eba3935c60f1214307ed48938179
gandu1962 <nuralam66668@gmail.com>
2025-09-01 21:48:57 -0600

    Update waifu.js

--- scripts/cmds/waifu.js
+++ scripts/cmds/waifu.js
@@ -1,60 +1,42 @@
 const axios = require('axios');
 
 module.exports = {
-	config: {
-		name: "waifu",
-		aliases: ["wife"],
-		version: "1.0",
-		author: "tas3n",
-		countDown: 6,
-		role: 0,
-		shortDescription: "get random waifu",
-		longDescription: "Get waifu neko: waifu, neko, shinobu, megumin, bully, cuddle, cry, kiss, lick, hug, awoo, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe",
-		category: "anime",
-		guide: "{pn} {{<name>}}"
-	}
+  config: {
+    name: "waifu",
+    aliases: ["wife"],
+    version: "1.0",
+    author: "tas3n",
+    countDown: 6,
+    role: 0,
+    shortDescription: "Get random waifu",
+    longDescription: "Get waifu: waifu, neko, shinobu, megumin, bully, cuddle, cry, kiss, lick, hug, awoo, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe",
+    category: "anime",
+    guide: "{pn} <name>"
+  }, // ✅ this comma is required
 
-	onStart: async function ({ message, args }) {
-		const name = args.join(" ");
-		if (!name)
+  onStart: async function ({ message, args }) {
+    const name = args.join(" ");
 
-			try {
-				let res = await axios.get(`https://api.waifu.pics/sfw/waifu`)
+    try {
+      // If no category given, default to "waifu"
+      const endpoint = name || "waifu";
+      const res = await axios.get(`https://api.waifu.pics/sfw/${endpoint}`);
+      const img = res.data.url;
 
+      const form = {
+        body: `   「 𝔀𝓪𝓲𝓯𝓾 」   `
+      };
 
-				let res2 = res.data
-				let img = res2.url
+      if (img) {
+        form.attachment = await global.utils.getStreamFromURL(img);
+      }
 
-				const form = {
-					body: `   「 𝔀𝓪𝓲𝓯𝓾  」   `
+      message.reply(form);
 
-				};
-				if (img)
-					form.attachment = await global.utils.getStreamFromURL(img);
-				message.reply(form);
-			} catch (e) {
-				message.reply(` Not Found`)
-			}
-
-
-		else {
-
-			try {
-				let res = await axios.get(`https://api.waifu.pics/sfw/${name}`)
-
-
-				let res2 = res.data
-				let img1 = res2.url
-
-				const form = {
-					body: `   「 𝔀𝓪𝓲𝓯𝓾  」   `
-
-				};
-				if (img1)
-					form.attachment = await global.utils.getStreamFromURL(img1);
-				message.reply(form);
-			} catch (e) { message.reply(` No waifu  \category: waifu, neko, shinobu, megumin, bully, cuddle, cry, kiss, lick, hug, awoo, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe `) }
-
-		}
-	}
+    } catch (e) {
+      message.reply(
+        `⚠️ Not Found\n\nAvailable categories:\nwaifu, neko, shinobu, megumin, bully, cuddle, cry, kiss, lick, hug, awoo, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe`
+      );
+    }
+  }
 };
